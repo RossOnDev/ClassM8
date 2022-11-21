cbeffcb3fcede7a1b1eee88903042c0a07be6355
Patrick <43619034+RossOnDev@users.noreply.github.com>
2022-11-21 13:03:52 +0100

    Created First User Class, Seperated CSS Files

---
+++ assets/js/models/User.js
@@ -0,0 +1,7 @@
+export default class User {
+    constructor(firstName, lastName, skills, email, profilePic) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.skills = skills;
+    }
+}
---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+import User from "./models/User.js";
+
+const user = new User("Patrick", "Rössler", ["IT", "DB"]);
+const user2 = new User("Alexander", "Steiner", ["IT", "Linux"]);
+
+
+function getSkills(users)
+{
+    return Skills; // ["IT", "Linux", "DB"]
+}
