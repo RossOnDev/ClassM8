1e3b67f14e40f892334a13cc1dd3481c1e566336
Patrick <43619034+RossOnDev@users.noreply.github.com>
2022-11-22 11:03:29 +0100

    Added Skills Checkboxen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,35 @@
 import User from "./models/User.js";
 
-const user = new User("Patrick", "Rössler", ["IT", "DB"]);
-const user2 = new User("Alexander", "Steiner", ["IT", "Linux"]);
+const users = [];
+
+const user = new User("Patrick", "Rössler", ["Informatik", "Datenbankdesign", "Softwareentwicklung"]);
+const user2 = new User("Alexander", "Steiner", ["Linux", "Mathematik"]);
+const user3 = new User("Alexander", "Steiner", ["Informatik", "Linux", "Datenbankdesign"]);
+
+users.push(user);
+users.push(user2);
+users.push(user3);
+
+function getSkillsListFromUsers(users) {
+    const skills = new Set();
+
+    users.forEach(user => {
+        user.skills.forEach(skill => skills.add(skill))
+    })
+
+    return skills;
+}
 
 
-document.getElementById("name").innerText = user.firstName + " " + user.lastName;
+const skills = getSkillsListFromUsers(users);
+
+let skillHTML = "";
+skills.forEach(skill => {
+    skillHTML += "<label>" +
+                 "    <input type=\"checkbox\">" +
+                 "    <span class=\"checkbox\"></span>" +
+                 "    <a>"+ skill +"</a>" +
+                 "</label>"
+})
+
+document.getElementById("skills").innerHTML = skillHTML;
