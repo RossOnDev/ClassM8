b4e775e055a924a83a4fc43f2c1bb5da0556a896
Patrick <43619034+RossOnDev@users.noreply.github.com>
2022-11-24 11:42:33 +0100

    getSelectedSkills added - not implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,32 +5,50 @@ const dataGenerator = new DataGenerator();
 const users = dataGenerator.getUsers();
 const skills = dataGenerator.getSkillsListFromUsers(users);
 
-let skillHTML = "";
-skills.forEach(skill => {
-    skillHTML += "<label>" +
-                 "    <input type=\"checkbox\">" +
-                 "    <span class=\"checkbox\"></span>" +
-                 "    <a>"+ skill +"</a>" +
-                 "</label>"
-})
+renderSkills(skills);
+renderCards(users);
+getSelectedSkills();
 
-document.getElementById("skills").innerHTML = skillHTML;
-
-let cardsHTML = "";
-users.forEach(user => {
-    const fullName = user.firstName + " " + user.lastName;
-    const profileUrl = "./profiles/" + user.firstName.toLowerCase() + "_" + user.lastName.toLowerCase() + ".html";
-
-    cardsHTML += "<div class=\"card\" onclick=\"window.location = '" + profileUrl + "'\">\n" +
-        "                    <img src=\"assets/img/avatars/avatar.jpg\" alt='Image of "+ fullName +"'>\n" +
-        "                    <div class=\"card-desc\">\n" +
-        "                        <h6>" + fullName +"</h6>\n" +
-        "                        <div class=\"skill-list\">\n";
-
-    user.skills.forEach(skill => {
-        cardsHTML += ("<a>" + skill + "</a>")
+/**
+ * @param skills    {string[] | Set<string>}
+ */
+function renderSkills(skills) {
+    let skillHTML = "";
+    skills.forEach(skill => {
+        skillHTML += "<label>" +
+            "    <input type=\"checkbox\" onchange='getSelectedSkills()'>" +
+            "    <span class=\"checkbox\"></span>" +
+            "    <a>"+ skill +"</a>" +
+            "</label>"
     })
 
-    cardsHTML += "</div></div></div>";
-})
-document.getElementById("cards").innerHTML = cardsHTML;
+    document.getElementById("skills").innerHTML = skillHTML;
+}
+
+/**
+ * @param users {User[]}
+ */
+function renderCards(users) {
+    let cardsHTML = "";
+    users.forEach(user => {
+        const fullName = user.firstName + " " + user.lastName;
+        const profileUrl = "./profiles/" + user.firstName.toLowerCase() + "_" + user.lastName.toLowerCase() + ".html";
+
+        cardsHTML += "<div class=\"card\" onclick=\"window.location = '" + profileUrl + "'\">\n" +
+            "                    <img src=\"assets/img/avatars/avatar.jpg\" alt='Image of "+ fullName +"'>\n" +
+            "                    <div class=\"card-desc\">\n" +
+            "                        <h6>" + fullName +"</h6>\n" +
+            "                        <div class=\"skill-list\">\n";
+
+        user.skills.forEach(skill => {
+            cardsHTML += ("<a>" + skill + "</a>")
+        })
+
+        cardsHTML += "</div></div></div>";
+    })
+    document.getElementById("cards").innerHTML = cardsHTML;
+}
+
+function getSelectedSkills() {
+    console.log("Ich bin selected Skills!")
+}
