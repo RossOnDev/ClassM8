2046397562aa0db5a2c86a0e10465f4ff5fecc88
Patrick <43619034+RossOnDev@users.noreply.github.com>
2022-12-12 12:08:34 +0100

    Dropdown for Mobile and Tablet, List for Desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,6 @@ function toggleMenuBar()
 {
     let menuItems = document.getElementById("menu-items");
 
-
     if (menuItems.style.left === "0px")
     {
         menuItems.style.left = "-100vw";
